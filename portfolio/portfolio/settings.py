e6cc6636888d5f8150200a4913690237cdd46a4e
JackNLewis <jacklewis2809@gmail.com>
2021-08-06 18:26:12 +0100

    added react page to cors whitelist

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -27,11 +27,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'jlewis2809.pythonanywhere.com',
+    "127.0.0.1",
+    "localhost:8000",
 ]
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-'jacknlewis.github.io/portfolio'
+'https://jacknlewis.github.io',
+ 'http://localhost:3000'
 )
 
 # Application definition
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
