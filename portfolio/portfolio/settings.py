17d1ffa942b078c1beedd54040756224e6f10579
JackNLewis <jacklewis2809@gmail.com>
2021-09-05 16:02:32 +0100

    addded pagnator class

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -144,6 +144,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,
+    'DEFAULT_PAGINATION_CLASS': 'blog.pagination.CustomPagination',
+    'PAGE_SIZE': 4,
 }
